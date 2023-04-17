8e63a01570ca15f29322b8bbf8e139a3430e32b9
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-04-18 06:32:28 +0900

    add: tailwind.css導入

--- src/index.php
+++ src/index.php
@@ -7,19 +7,18 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>POSSE 初めてのWeb制作</title>
   <!-- スタイルシート読み込み -->
-  <link rel="stylesheet" href="./assets/styles/common.css">
+  <link rel="stylesheet" href="./user/assets/styles/common.css">
   <!-- Google Fonts読み込み -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link
     href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Plus+Jakarta+Sans:wght@400;700&display=swap"
     rel="stylesheet">
-  <script src="../assets/scripts/common.js" defer></script>
 </head>
 
 <body>
   <header id="js-header" class="l-header p-header">
-    <div class="p-header__logo"><img src="./assets/img/logo.svg" alt="POSSE"></div>
+    <div class="p-header__logo"><img src="./user/assets/img/logo.svg" alt="POSSE"></div>
     <button class="p-header__button" id="js-headerButton"></button>
     <div class="p-header__inner">
       <nav class="p-header__nav">
@@ -33,7 +32,8 @@
         </ul>
       </nav>
       <div class="p-header__official">
-        <a href="https://line.me/R/ti/p/@651htnqp?from=page" target="_blank" rel="noopener noreferrer" class="p-header__official__link--line">
+        <a href="https://line.me/R/ti/p/@651htnqp?from=page" target="_blank" rel="noopener noreferrer"
+          class="p-header__official__link--line">
           <i class="u-icon__line"></i>
           <span class="">POSSE公式LINEを追加</span>
           <i class="u-icon__link"></i>
@@ -42,8 +42,8 @@
       </div>
       <ul class="p-header__sns p-sns">
         <li class="p-sns__item">
-          <a href="https://twitter.com/posse_program" target="_blank" rel="noopener noreferrer" class="p-sns__item__link"
-            aria-label="Twitter">
+          <a href="https://twitter.com/posse_program" target="_blank" rel="noopener noreferrer"
+            class="p-sns__item__link" aria-label="Twitter">
             <i class="u-icon__twitter"></i>
           </a>
         </li>
@@ -64,9 +64,10 @@
         <div class="p-top-hero__body">
           <h1 class="p-top-hero__body__title">学生プログラミングコミュニティ POSSE（ポッセ）</h1>
           <span class="p-top-hero__body__catchcopy">自分史上最高<br>を仲間と。</span>
+          <h1 class="text-orange-500">TEST</h1>
         </div>
-        <picture class="p-top-hero__image">
-          <img src="./assets/img/img-hero.jpg" alt="">
+        <picture class=" p-top-hero__image">
+          <img src="./user/assets/img/img-hero.jpg" alt="">
         </picture>
         <div class="p-top-hero__scroll">Scroll Down</div>
       </div>
@@ -81,7 +82,7 @@
           </h2>
           <div class="p-top-about__body">
             <picture class="p-top-about__image">
-              <img src="./assets/img/img-about.jpg" alt="POSSEメンバー集合写真">
+              <img src="./user/assets/img/img-about.jpg" alt="POSSEメンバー集合写真">
             </picture>
             <div class="p-top-about__content">
               <p>
@@ -126,7 +127,7 @@
     <div class="l-footer__inner">
       <div class="p-footer__siteinfo">
         <span class="p-footer__logo">
-          <img src="./assets/img/logo.svg" alt="POSSE">
+          <img src="./user/assets/img/logo.svg" alt="POSSE">
         </span>
         <a href="https://posse-ap.com/" target="_blank" rel="noopener noreferrer"
           class="p-footer__siteinfo__link">POSSE公式サイト</a>
