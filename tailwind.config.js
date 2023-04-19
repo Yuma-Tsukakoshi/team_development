0a8a6cd315aa71608b1dd01b57a68b2b1dcc004d
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-04-20 05:43:07 +0900

    add:developブランチ内容反映

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,php}"],
+  content: [
+    "./src/**/*.js",
+    "./src/**/*.html",
+    "./src/**/*.php",
+  ], 
+  // dockerにnpm入れる  
   theme: {
     extend: {},
   },
