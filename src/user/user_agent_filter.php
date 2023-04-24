8a5018ca49bfb86c935222a05b3bf73823a1887d
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-04-24 09:16:19 +0900

    add:絞り込み機能実装

--- src/user/user_agent_filter.php
+++ src/user/user_agent_filter.php
@@ -2,9 +2,25 @@
 
 require_once(dirname(__FILE__) . '/../dbconnect.php');
 $pdo = Database::get();
-
 $agents = $pdo->query("SELECT * FROM clients")->fetchAll(PDO::FETCH_ASSOC);
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // チェックボックスから受信した値を取得
+    $filters = $_POST['filter'];
+    // 絞り込みを行うSQLクエリを作成
+    $query = "SELECT * FROM clients AS main INNER JOIN (SELECT DISTINCT sub.client_id FROM clients AS sub INNER JOIN label_client_relation ON sub.client_id=label_client_relation.client_id WHERE";
+    foreach($filters as $index => $filter) {
+        if($index > 0) {
+            $query .= " OR";
+        }
+        $query .= " label_id = $filter";
+    }
+    $query .= ") AS sub ON main.client_id=sub.client_id";
+    // クエリを実行する
+    $statement = $pdo->query($query);
+    $agents = $statement->fetchAll();
+}
 
+// SELECT * FROM clients AS main INNER JOIN (SELECT DISTINCT sub.client_id FROM clients AS sub INNER JOIN label_client_relation ON sub.client_id=label_client_relation.client_id WHERE label_id = 1 OR label_id = 4 ) AS sub ON main.client_id=sub.client_id;
 
 ?>
--- src/user/user_agent_list.php
+++ src/user/user_agent_list.php
@@ -2,7 +2,6 @@
 require_once(dirname(__FILE__) . '/../dbconnect.php');
 require_once(dirname(__FILE__) . '/user_agent_filter.php');
 
-
 $pdo = Database::get();
 $labels = $pdo->query("SELECT * FROM labels")->fetchAll(PDO::FETCH_ASSOC);
 $agent_labels = $pdo->query("SELECT * FROM label_client_relation INNER JOIN labels ON label_client_relation.label_id = labels.label_id")->fetchAll(PDO::FETCH_ASSOC);
@@ -30,111 +29,72 @@ $agent_labels = $pdo->query("SELECT * FROM label_client_relation INNER JOIN labe
     </div>
     <div>
       <img src="" alt=" shopping_cart">
-      < /div>
-        < /section>
+    </div>
+  </section>
 
-          < main class="grid grid-cols-2">
-            < div class="m-8">
-              < div class="major">
-                < !--font - awsomeで入れてく-- ->
-                  < h2> 専攻 < /h2>
-                      <?php for($i=1;$i<=2;$i++){ ?>
-                      < input type="checkbox" id="major<?=$i?>" class="check-label" value="<?=$labels
-        [$i-1]["label_name"] ?>">
-                        < label for="major<?=$i?>" class="label-hover"> <?=$labels[$i-1]["label_name"] ?> < /label>
-                            <?php }?>
-                            < /div>
-                              < div class=" contact">
-                                < h2> 面談方法 < /h2>
-                                    <?php for($i=3;$i<=5;$i++){ ?>
-                                    < input type="checkbox" id="contact<?=$i?>" class="check-label"
-                                      value="<?=$labels[$i-1]["label_name"] ?>">
-                                      < label for="contact<?=$i?>" class="label-hover">
-                                        <?= $labels[$i-1]["label_name"] ?> < /label>
-                                          <?php }?>
-                                          < /div>
-                                            < div class="area">
-                                              < h2> エリア < /h2>
-                                                  <?php for($i=6;$i<=9;$i++){ ?>
-                                                  < input type="checkbox" id="area<?=$i?>" class="check-label"
-                                                    value="<?=$labels[$i-1]["label_name"] ?>">
-                                                    < label for="area<?=$i?>" class="label-hover">
-                                                      <?= $labels[$i-1]["label_name"] ?> < /label>
-                                                        <?php }?>
-                                                        < /div>
-                                                          < button class="btn-big blue"> 検索 < /button>
-                                                              < /div>
-                                                                < div>
-                                                                  < h3>
-                                                                    < span> 6 < /span>件ヒット</h3>
-                                                                        < div class="agent-list">
-                                                                          < div>
-                                                                            <?php foreach ($agents as $key => $agent) { ?>
-                                                                            < div class="top">
-                                                                              < img src="<?=$agent["logo_img"]?>"
-                                                                                alt="エージェント画像">
-                                                                                < div>
-                                                                                  < h2> <?=$agent["agent_name"]?> < /h2>
-                                                                                      < div>
-                                                                                        < h3>
-                                                                                          <?=$agent["service_name"]?> <
-                                                                                            /h3>
-                                                                                            < p>
-                                                                                              <?=$agent["recommend_point1"]?>
-                                                                                              < /p>
-                                                                                                < p>
-                                                                                                  <?=$agent["recommend_point2"]?>
-                                                                                                  < /p>
-                                                                                                    < p>
-                                                                                                      <?=$agent["recommend_point3"]?>
-                                                                                                      < /p>
-                                                                                                        < /div>
-                                                                                                          < /div>
-                                                                                                            < /div>
-                                                                                                              < div
-                                                                                                                class="bottom">
-                                                                                                                < div
-                                                                                                                  class="labels">
-                                                                                                                  <?php foreach($agent_labels as $agent_label){ ?>
-                                                                                                                  <?php if($agent_label["client_id"]==$key+1){?>
-                                                                                                                  <
-                                                                                                                    span>
-                                                                                                                    <?=$agent_label["label_name"]?>
-                                                                                                                    <
-                                                                                                                      /span>
-                                                                                                                      <?php }?>
-                                                                                                                      <?php }?>
-                                                                                                                      <
-                                                                                                                        /div>
-                                                                                                                        < div
-                                                                                                                          class="block">
-                                                                                                                          < button
-                                                                                                                            class="btn-big blue"
-                                                                                                                            id="cart<?=$key+1?>">
-                                                                                                                            カートに追加する
-                                                                                                                            <
-                                                                                                                              /button>
-                                                                                                                              < button
-                                                                                                                                class="btn-big blue"
-                                                                                                                                id="agent<?=$key+1?>">
-                                                                                                                                詳細を見る→
-                                                                                                                                <
-                                                                                                                                  /button>
-                                                                                                                                  <
-                                                                                                                                    /div>
-                                                                                                                                    <
-                                                                                                                                      /div>
-                                                                                                                                      <?php }?>
-                                                                                                                                      <
-                                                                                                                                        /div>
-                                                                                                                                        <
-                                                                                                                                          /div>
-                                                                                                                                          <
-                                                                                                                                            /div>
-                                                                                                                                            <
-                                                                                                                                              /main>
-                                                                                                                                              <
-                                                                                                                                                /body>
+  <main class="grid grid-cols-2">
+    <form method="post" action="">
+      <div class="major">
+        <h2> 専攻 </h2>
+        <?php for($i=1;$i<=2;$i++){ ?>
+        <input type="checkbox" id="major<?=$i?>" class="check-label" name="filter[]" value="<?=$labels
+        [$i-1]["label_id"] ?>">
+        <label for="major<?=$i?>" class="label-hover"><?=$labels[$i-1]["label_name"] ?> </label>
+        <?php }?>
+      </div>
+      <div class=" contact">
+        <h2> 面談方法 </h2>
+        <?php for($i=3;$i<=5;$i++){ ?>
+        <input type="checkbox" id="contact<?=$i?>" class="check-label" name="filter[]" value="<?=$labels[$i-1]["label_id"] ?>">
+        <label for="contact<?=$i?>" class="label-hover"><?= $labels[$i-1]["label_name"] ?> </label>
+        <?php }?>
+      </div>
+      <div class="area">
+        <h2> エリア </h2>
+        <?php for($i=6;$i<=9;$i++){ ?>
+        <input type="checkbox" id="area<?=$i?>" class="check-label" name="filter[]" value="<?=$labels[$i-1]["label_id"] ?>">
+        <label for="area<?=$i?>" class="label-hover"><?= $labels[$i-1]["label_name"] ?></label>
+        <?php }?>
+      </div>
+      <button class="btn-big blue">検索</button>
+    </form>
+    <div>
+      <h3><span>6</span>件ヒット</h3>
+      <div class="agent-list">
+        <div>
+          <?php foreach ($agents as $key => $agent) { ?>
+          <div class="top">
+            <img src="<?=$agent["logo_img"]?>" alt="エージェント画像">
+            <div>
+              <h2><?=$agent["agent_name"]?></h2>
+              <div>
+                <h3><?=$agent["service_name"]?></h3>
+                <p><?=$agent["recommend_point1"]?></p>
+                <p><?=$agent["recommend_point2"]?></p>
+                <p><?=$agent["recommend_point3"]?></p>
+              </div>
+            </div>
+          </div>
+          <div class="bottom">
+            <div class="labels">
+              <?php foreach($agent_labels as $agent_label){ ?>
+              <?php if($agent_label["client_id"]==$key+1){?>
+              <span>
+                <?=$agent_label["label_name"]?>
+              </span>
+              <?php }?>
+              <?php }?>
+            </div>
+            <div class="block">
+              <button class="btn-big blue" id="cart<?=$key+1?>">カートに追加する</button>
+              <button class="btn-big blue" id="agent<?=$key+1?>">詳細を見る→</button>
+            </div>
+          </div>
+          <?php }?>
+        </div>
+      </div>
+    </div>
+  </main>
+</body>
 
-                                                                                                                                                <
-                                                                                                                                                  /html>
+</html>
