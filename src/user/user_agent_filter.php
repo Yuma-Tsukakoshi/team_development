ebf58cd02b2935a36de6b0c71ecbc32807746bc3
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-04-24 10:28:07 +0900

    fix:emptyの時のエラー処理

--- src/user/user_agent_filter.php
+++ src/user/user_agent_filter.php
@@ -4,7 +4,7 @@ require_once(dirname(__FILE__) . '/../dbconnect.php');
 $pdo = Database::get();
 $agents = $pdo->query("SELECT * FROM clients")->fetchAll(PDO::FETCH_ASSOC);
 
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
+if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST['filter'])){
     // チェックボックスから受信した値を取得
     $filters = $_POST['filter'];
     // 絞り込みを行うSQLクエリを作成
@@ -24,3 +24,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 }
 
 ?>
+<!-- SELECT main.client_id,main.service_name FROM clients AS main INNER JOIN (SELECT DISTINCT sub.client_id FROM clients AS sub INNER JOIN label_client_relation ON sub.client_id=label_client_relation.client_id WHERE label_id = 1 OR label_id = 4 ) AS sub ON main.client_id=sub.client_id; -->
+<!-- SELECT service_name , labels.label_id ,labels.label_name FROM clients INNER JOIN label_client_relation ON clients.client_id=label_client_relation.client_id
+INNER JOIN labels ON label_client_relation.label_id=labels.label_id; -->
