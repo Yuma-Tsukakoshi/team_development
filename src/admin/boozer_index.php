66d071072d4234bd5fd1ffb287a97c9857d2eebd
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-04-26 16:04:03 +0900

    add:管理者画面のテンプレ追加

--- src/admin/boozer_index.php
+++ src/admin/boozer_index.php
@@ -9,5 +9,163 @@ require_once(dirname(__FILE__) . '/../dbconnect.php');
 // }else{
 //     $is_empty = count($questions) === 0;
 // }
-
 ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet"
+    />
+    <link rel="stylesheet" href="../vendor/tailwind/tailwind.output.css">
+    <title>boozer管理者画面</title>
+  </head>
+  <body>
+    <div
+      class="flex h-screen bg-gray-50"
+      :class="{ 'overflow-hidden': isSideMenuOpen}"
+    >
+      <!-- side banner -->
+      <aside class="z-20 flex-shrink-0 hidden w-64 overflow-y-auto bg-slate-500 md:block" >
+        <div class="py-4 text-gray-500">
+          <a
+            class="ml-6 text-lg font-bold text-gray-800 "
+            href="#"
+          >
+            SideBanner
+          </a>
+          <ul class="mt-6">
+            <li class="relative px-6 py-3">
+              <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800" href="#">
+                <span class="ml-4">企業一覧</span>
+              </a>
+            </li>
+            <li class="relative px-6 py-3">
+              <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800" href="#">
+                <span class="ml-4">企業新規登録</span>
+              </a>
+            </li>
+            <li class="relative px-6 py-3">
+              <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800" href="#">
+                <span class="ml-4">無効申請一覧</span>
+                <!-- 無効申請で絞り込みする -->
+              </a>
+            </li>
+          </ul>
+        </div>
+      </aside>
+
+      <div class="flex flex-col flex-1 w-full">
+        <main class="h-full pb-16 overflow-y-auto">
+          <div class="container grid px-6 mx-auto">
+            <h2 class="my-6 text-2xl font-semibold text-gray-700 ">企業一覧</h2>
+            <div class="w-full overflow-hidden rounded-lg shadow-xs">
+              <div class="w-full overflow-x-auto">
+                <table class="w-full whitespace-no-wrap">
+                  <thead>
+                    <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b">
+                      <th class="px-4 py-3">企業名</th>
+                      <th class="px-4 py-3">掲載期間</th>
+                      <th class="px-4 py-3">登録状態</th>
+                      <th class="px-4 py-3">操作</th>
+                    </tr>
+                  </thead>
+                  <tbody
+                    class="bg-white divide-y"
+                  >
+                    <tr class="text-gray-700">
+                      <td class="px-4 py-3">
+                        <p class="font-semibold items-center text-sm">企業名</p>
+                      </td>
+                      <td class="px-4 py-3 text-sm">
+                        掲載期間:started~:ended
+                      </td>
+                      <td class="px-4 py-3 text-xs">
+                        <span
+                          class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full"
+                        >
+                        <!-- 色の設定はクラスの付加でjqueryで行う -->
+                          Approved
+                        </span>
+                      </td>
+                      <td class="px-4 py-3">
+                        <div class="flex items-center space-x-4 text-sm">
+                          <button
+                            class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-blue-500 rounded-lg focus:outline-none focus:shadow-outline-gray"
+                            aria-label="Edit"
+                          >
+                            詳細
+                            <!-- 詳細押した後に編集できるように -->
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div
+                class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t sm:grid-cols-9"
+              >
+                <span class="flex items-center col-span-3">
+                  Showing 21-30 of 100
+                </span>
+                <span class="col-span-2"></span>
+                <!-- Pagination -->
+                <span class="flex col-span-4 mt-2 sm:mt-auto sm:justify-end">
+                  <nav aria-label="Table navigation">
+                    <ul class="inline-flex items-center">
+                      <li>
+                        <button
+                          class="px-3 py-1 rounded-md rounded-l-lg focus:outline-none focus:shadow-outline-purple"
+                          aria-label="Previous"
+                        >
+                          <svg
+                            class="w-4 h-4 fill-current"
+                            aria-hidden="true"
+                            viewBox="0 0 20 20"
+                          >
+                            <path
+                              d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                              clip-rule="evenodd"
+                              fill-rule="evenodd"
+                            ></path>
+                          </svg>
+                        </button>
+                      </li>
+                      <li>
+                        <button
+                          class="px-3 py-1 rounded-md focus:outline-none focus:shadow-outline-purple"
+                        >1 <!-- ■１ ■２ ■3 ... みたいな -->
+                        </button>
+                      </li>
+                      <li>
+                        <button
+                          class="px-3 py-1 rounded-md rounded-r-lg focus:outline-none focus:shadow-outline-purple"
+                          aria-label="Next"
+                        >
+                          <svg
+                            class="w-4 h-4 fill-current"
+                            aria-hidden="true"
+                            viewBox="0 0 20 20"
+                          >
+                            <path
+                              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                              clip-rule="evenodd"
+                              fill-rule="evenodd"
+                            ></path>
+                          </svg>
+                        </button>
+                      </li>
+                    </ul>
+                  </nav>
+                </span>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  </body>
+  </body>
+</html>
