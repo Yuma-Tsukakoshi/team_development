f2669ab454caaf283af0c9dd927657a8e71008fb
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-04-26 13:42:09 +0900

    add: sesesionブース

--- src/admin/boozer_index.php
+++ src/admin/boozer_index.php
@@ -1,1 +1,13 @@
+<?php
 
+// session_start();
+require_once(dirname(__FILE__) . '/../dbconnect.php');
+
+// if (!isset($_SESSION['id'])) {
+//     header('Location: http://localhost:8080/admin/boozer_auth/signup.php');
+//     exit();
+// }else{
+//     $is_empty = count($questions) === 0;
+// }
+
+?>
