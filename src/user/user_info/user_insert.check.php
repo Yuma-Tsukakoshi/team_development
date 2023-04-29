dbb57bb9031e3710a22236a6cd4ca09c0d5660d1
平手美羽 <miyuh1@MacBook-Air-4.local>
2023-04-29 16:12:27 +0900

    メール通知機能修正段階

--- src/user/user_info/user_insert.check.php
+++ src/user/user_info/user_insert.check.php
@@ -1,6 +1,3 @@
-<?php
-  
-?>
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -20,85 +17,81 @@
 
 <body>
   <main>
+  <form action="http://localhost:8080/user/user_info/user_insert.done.php" method="post">
     <h1>お申し込み確認フォーム</h1>
-      <div>
-        <label for="name" class="form-label">名前:</label>
-        <input type="text" name="name" id="name" class="form-control" value="<?=$_POST['name']?>" disabled/>
-        <button class="edit-button" data-value="2">編集</button>
-      </div>
-      <label for="hurigana" class="form-label">ふりがな:</label>
-        <input type="text" name="hurigana" id="hurigana" class="form-control" value="<?=$_POST['hurigana']?>" disabled/>
-        <button class="edit-button" data-value="2">編集</button>
-      </div>
-      <div class="form-controll">
-        <label for="email" class="form-label">メールアドレス:</label>
-        <input type="email" name="email" id="email" class="form-control" value="<?=$_POST['email']?>" disabled/>
-        <button class="edit-button" data-value="3">編集</button>
-      </div>
-      <div class="form-controll">
-        <label for="phone" class="form-label">電話番号:</label>
-        <input type="tel" name="phone" id="phone" class="form-control" pattern="\d{1,5}-\d{1,4}-\d{4,5}" title="電話番号は、市外局番からハイフン（-）を入れて記入してください。" value="<?=$_POST['phone']?>" disabled/>
-        <button class="edit-button" data-value="4">編集</button>
-      </div>
-      <div class="form-controll">
-        <fieldset class="form-control" disabled>
-        <label for="sex" class="form-label">性別:</label>
-        <input type="radio" name="sex" id="sex" value="女"  <?php if( $_POST['sex'] == '女'){?>checked<?php }?>/>女性
-        <input type="radio" name="sex" id="sex" value="男" <?php if( $_POST['sex'] == '男'){?>checked<?php }?> />男性
-        <input type="radio" name="sex" id="sex" value="その他" <?php if( $_POST['sex'] == 'その他'){?>checked<?php }?>/>その他
-        </fieldset>
-        <button class="edit-button" data-value="5">編集</button>
-
-      </div>
-      <div class="form-controll">
-        <label for="birthday" class="form-label">生年月日:</label>
-        <input type="date" name="birthday" id="birthday" class="form-control" value="<?=$_POST['birthday']?>" disabled/>
-        <button class="edit-button" data-value="6">編集</button>
-      </div>
-      <div class="form-controll">
-        <!--自動入力-->
-        <fieldset class="form-control" disabled>
-          <!-- <label for="address" class="form-label">郵便番号:</label>
-          <input type="text" id="address" name="address" onKeyUp="$('#address').zip2addr('#address_detail');" value="<?=$_POST['address']?>" ><br/>
-          <label for="address_detail" class="form-label">住所:</label>
-          <input type="text" name="address_detail" id="address_detail" value="<?=$_POST['address_detail']?>" > -->
-          <label for="prefecture" class="form-label">住まいの都道府県:</label>
-          <input type="text" name="prefecture" id="prefecture" value="<?=$_POST['prefecture']?>">
-        </fieldset>
-        <button class="edit-button" data-value="7">編集</button>
-      </div>
-      <div class="form-controll">
-        <label for="college" class="form-label">大学名:</label>
-        <input type="text" name="college" id="college" class="form-control" value="<?=$_POST['college']?>" disabled/>
-        <button class="edit-button" data-value="8">編集</button>
-      </div>
-      <div class="form-controll">
-        <fieldset class="form-control" disabled>
-        <label for="faculty" class="form-label">学部:</label>
-        <input type="text" name="faculty" id="faculty"  value="<?=$_POST['faculty']?>" />
-        <label for="department" class="form-label">学科:</label>
-        <input type="text" name="department" id="department"  value="<?=$_POST['department']?>" />
-
-        </fieldset>
-        <button class="edit-button" data-value="9">編集</button>
-      </div>
-      <div class="form-controll">
-        <fieldset class="form-control" disabled>
-          <label for="division" class="form-label">文理:</label>
-          <input type="radio" name="division" id="division"  value="理系" <?php if( $_POST['division'] == '文系'){?>checked<?php }?> />文系
-          <input type="radio" name="division" id="division"
-          value="理系" <?php if( $_POST['division'] == '理系'){?>checked<?php }?> />理系
-        </fieldset>
-        <button class="edit-button" data-value="10">編集</button>
-      </div>
-      <div class="form-controll">
-        <label for="grad_year" class="form-label">卒業年度:</label>
-        <input type="text" name="grad_year" id="grad_year" class="form-control" value="<?=$_POST['grad_year']?>" disabled/>
-        <button class="edit-button" data-value="11">編集</button>
-
-      </div>
-      <input type="submit" id="submit-button" value="送信">
-      
+        <div>
+          <label for="name" class="form-label">名前:</label>
+          <input type="text" name="name" id="name" class="form-control" value="<?=$_POST['name']?>" readonly/>
+          <button class="edit-button" data-value="2">編集</button>
+        </div>
+        <label for="hurigana" class="form-label">ふりがな:</label>
+          <input type="text" name="hurigana" id="hurigana" class="form-control" value="<?=$_POST['hurigana']?>" readonly/>
+          <button class="edit-button" data-value="2">編集</button>
+        </div>
+        <div class="form-controll">
+          <label for="email" class="form-label">メールアドレス:</label>
+          <input type="email" name="email" id="email" class="form-control" value="<?=$_POST['email']?>" readonly/>
+          <button class="edit-button" data-value="3">編集</button>
+        </div>
+        <div class="form-controll">
+          <label for="phone" class="form-label">電話番号:</label>
+          <input type="tel" name="phone" id="phone" class="form-control" pattern="\d{1,5}-\d{1,4}-\d{4,5}" title="電話番号は、市外局番からハイフン（-）を入れて記入してください。" value="<?=$_POST['phone']?>" readonly/>
+          <button class="edit-button" data-value="4">編集</button>
+        </div>
+        <div class="form-controll">
+          <fieldset class="form-control" readonly>
+          <label for="sex" class="form-label">性別:</label>
+          <input type="radio" name="sex" id="sex" value="女"  <?php if( $_POST['sex'] == '女'){?>checked<?php }?>/>女性
+          <input type="radio" name="sex" id="sex" value="男" <?php if( $_POST['sex'] == '男'){?>checked<?php }?> />男性
+          <input type="radio" name="sex" id="sex" value="その他" <?php if( $_POST['sex'] == 'その他'){?>checked<?php }?>/>その他
+          </fieldset>
+          <button class="edit-button" data-value="5">編集</button>
+  
+        </div>
+        <div class="form-controll">
+          <label for="birthday" class="form-label">生年月日:</label>
+          <input type="date" name="birthday" id="birthday" class="form-control" value="<?=$_POST['birthday']?>" readonly/>
+          <button class="edit-button" data-value="6">編集</button>
+        </div>
+        <div class="form-controll">
+          <!--自動入力-->
+          <fieldset class="form-control" readonly>
+            <label for="prefecture" class="form-label">住まいの都道府県:</label>
+            <input type="text" name="prefecture" id="prefecture" value="<?=$_POST['prefecture']?>">
+          </fieldset>
+          <button class="edit-button" data-value="7">編集</button>
+        </div>
+        <div class="form-controll">
+          <label for="college" class="form-label">大学名:</label>
+          <input type="text" name="college" id="college" class="form-control" value="<?=$_POST['college']?>" readonly/>
+          <button class="edit-button" data-value="8">編集</button>
+        </div>
+        <div class="form-controll">
+          <fieldset class="form-control" readonly>
+          <label for="faculty" class="form-label">学部:</label>
+          <input type="text" name="faculty" id="faculty"  value="<?=$_POST['faculty']?>" />
+          <label for="department" class="form-label">学科:</label>
+          <input type="text" name="department" id="department"  value="<?=$_POST['department']?>" />
+  
+          </fieldset>
+          <button class="edit-button" data-value="9">編集</button>
+        </div>
+        <div class="form-controll">
+          <fieldset class="form-control" readonly>
+            <label for="division" class="form-label">文理:</label>
+            <input type="radio" name="division" id="division"  value="理系" <?php if( $_POST['division'] == '文系'){?>checked<?php }?> />文系
+            <input type="radio" name="division" id="division"
+            value="理系" <?php if( $_POST['division'] == '理系'){?>checked<?php }?> />理系
+          </fieldset>
+          <button class="edit-button" data-value="10">編集</button>
+        </div>
+        <div class="form-controll">
+          <label for="grad_year" class="form-label">卒業年度:</label>
+          <input type="text" name="grad_year" id="grad_year" class="form-control" value="<?=$_POST['grad_year']?>" readonly/>
+          <button class="edit-button" data-value="11">編集</button>  
+        </div>
+        <input type="submit" id="submit-button" value="送信">  
+      </form>  
   </main>
 </body>
 </html>
--- src/user/user_info/user_insert.done.php
+++ src/user/user_info/user_insert.done.php
@@ -1,36 +1,60 @@
-<?php
-require_once(dirname(__FILE__) . './../../dbconnect.php');
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>申し込みフォーム完了</title>
+</head>
+<body>
+  <h1>申し込みが完了しました。</h1>
+  <p>以下の内容でメールを送信しました。</p>
 
-session_start();
-$pdo=Database::get();
-try {
-  $pdo->beginTransaction();
-  $stmt = $pdo->prepare("INSERT INTO users(name,hurigana,sex,birthday,college,faculty,department,division,grad_year,prefecture,mail,phone) VALUES(:name,:hurigana,:sex,:birthday,:college,:faculty,:department,:division,:grad_year,:prefecture,:mail,:phone)");
-  $stmt->execute([
-    "name" => $_POST["name"],
-    "hurigana" => $_POST['hurigana'],
-    "sex" => $_POST["sex"],
-    "birthday" => $_POST["birthday"],
-    "college" => $_POST["college"],
-    "faculty" => $_POST["faculty"],
-    "department" => $_POST["department"],
-    "division" => $_POST["division"],
-    "grad_year" => $_POST["grad_year"],
-    "prefecture" => $_POST["prefecture"],
-    "mail" => $_POST["email"],
-    "phone" => $_POST["phone"],
-  ]);
-    
-  $pdo->commit();
+  <?php
+// POSTデータを取得
+$name = $_POST['name'];
+$hurigana = $_POST['hurigana'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$sex = $_POST['sex'];
+$birthday = $_POST['birthday'];
+$prefecture = $_POST['prefecture'];
+$college = $_POST['college'];
+$faculty = $_POST['faculty'];
+$department = $_POST['department'];
+$division = $_POST['division'];
+$grad_year = $_POST['grad_year'];
 
-  header('Content-type:application/json; charset=utf8');
-  $result='投稿が完了しました';
-  echo json_encode($result);
+// 宛先のメールアドレス
+$to = "tomato.mh.1215@gmail.com";
 
+// 件名
+$subject = "申し込みフォーム";
 
-}catch(\Exception $e){
-  $pdo->rollBack();
-  exit($e->getMessage());
+// メッセージ本文
+$message = "名前: {$name}\n";
+$message .= "ふりがな: {$hurigana}\n";
+$message .= "メールアドレス: {$email}\n";
+$message .= "電話番号: {$phone}\n";
+$message .= "性別: {$sex}\n";
+$message .= "生年月日: {$birthday}\n";
+$message .= "住まいの都道府県: {$prefecture}\n";
+$message .= "大学名: {$college}\n";
+$message .= "学部: {$faculty}\n";
+$message .= "学科: {$department}\n";
+$message .= "文理: {$division}\n";
+$message .= "卒業年度: {$grad_year}\n";
+
+// 送信元のメールアドレス
+$from = "miyuhirate@gmail.com";
+
+// 追加ヘッダー情報
+$headers = "From:" . $from;
+
+// メールの送信
+if(mail($to, $subject, $message, $headers)) {
+  echo "メールが送信されました。";
+} else {
+  echo "メールの送信に失敗しました。";
 }
-
 ?>
