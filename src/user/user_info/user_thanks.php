030872a800d2479da8b399e18459c5441f15fc76
平手美羽 <miyuh1@MacBook-Air-4.local>
2023-04-30 09:39:37 +0900

    ユーザー,アドミン,クライエントメール通知機能（編集もできる）

--- src/user/user_info/user_thanks.php
+++ src/user/user_info/user_thanks.php
@@ -11,35 +11,55 @@
 
 
 <?php
-// 宛先のメールアドレス
-$to = "user@mail.com";
-
-// 件名
-$subject = "【株式会社boozer】お申し込みありがとうございます";
-
-// メッセージ本文
-
-$message = "※このメールはシステムからの自動返信です\n\n";
-$message .= "お世話になっております。\n";
-$message .= "株式会社boozerへのお問い合わせありがとうございました。\n\n";
-$message .= "以下の内容でお問い合わせを受け付けいたしました。\n";
-$message .= "お手数ですがお間違いないかご確認ください。\n\n";
-$message .= "●営業日以内に、担当者よりご連絡いたしますので\n";
-$message .= "今しばらくお待ちくださいませ。\n\n";
 
 // 送信元のメールアドレス
-$from = "client@mail.com";
+$from = "craft@mail.com";
 
 // 追加ヘッダー情報
 $headers = "From:" . $from;
 
-// メールの送信
-if(mail($to, $subject, $message, $headers)) {
-  echo "<p>メールが送信されました。</p>";
-} else {
-  echo "メールの送信に失敗しました。\n";
-  echo "エラー情報: " . error_get_last()['message'];
+// 宛先と件名、メッセージをそれぞれ設定してメール送信関数を呼び出す
+function send_email($to, $subject, $message, $headers) {
+  if (mail($to, $subject, $message, $headers)) {
+  } else {
+    echo "メールの送信に失敗しました。\n";
+    echo "エラー情報: " . error_get_last()['message'];
+  }
 }
+
+// user@mail.comへのメール
+$to_user = "user@mail.com";
+$subject_user = "【株式会社boozer】お申し込みありがとうございます";
+$subject_user = "【株式会社boozer】お申し込みありがとうございます";
+$message_user = "※このメールはシステムからの自動返信です\n\n";
+$message_user .= "お世話になっております。\n";
+$message_user .= "株式会社boozerへのお問い合わせありがとうございました。\n\n";
+$message_user .= "以下の内容でお問い合わせを受け付けいたしました。\n";
+$message_user .= "お手数ですがお間違いないかご確認ください。\n\n";
+$message_user .= "●営業日以内に、担当者よりご連絡いたしますので\n";
+$message_user .= "今しばらくお待ちくださいませ。\n\n";
+
+send_email($to_user, $subject_user, $message_user, $headers);
+
+// client@mail.comへのメール
+$to_client = "client@mail.com";
+$subject_client = "【株式会社boozer】学生登録のお知らせ";
+$message_client = "お世話になっております。\n";
+$message_client .= "株式会社boozerでございます。\n\n";
+$message_client .= "CRAFTを通して学生より貴社の就活エージェントにお申込みいただいたことを通知いたします。詳しくはCRAFTよりご覧ください。\n\n";
+$message_client .= "なお、営業時間は平日9時〜18時となっております。\n";
+$message_client .= "時間外のお問い合わせは翌営業日にご連絡差し上げます。\n\n";
+$message_client .= "ご理解・ご了承の程よろしくお願い致します。";
+
+send_email($to_client, $subject_client, $message_client, $headers);
+
+// admin@mail.comへのメール
+$to_admin = "admin@mail.com";
+$subject_admin = "【株式会社boozer】学生登録のお知らせ";
+$message_admin = "学生が登録をしました。";
+
+send_email($to_admin, $subject_admin, $message_admin, $headers);
+
 ?>
 
 <a href="../assets/index.html"><p>トップに戻る</p></a>
