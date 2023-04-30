0ea6a8356a3cf12adc7966c3d8e818ecbf53dc33
平手美羽 <miyuh1@MacBook-Air-4.local>
2023-04-30 09:57:29 +0900

    ユーザー,アドミン,クライエントメール通知機能（編集もできる） thanksページのlink rel 足した

--- src/user/user_info/user_thanks.php
+++ src/user/user_info/user_thanks.php
@@ -5,6 +5,16 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>申し込み完了</title>
+    <!-- スタイルシート読み込み -->
+    <link rel="stylesheet" href="./user/assets/styles/common.css">
+  <!-- Google Fonts読み込み -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Plus+Jakarta+Sans:wght@400;700&display=swap"
+    rel="stylesheet">
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script type="text/javascript" src="./../assets/js/jquery.zip2addr.js"></script>
 </head>
 <body>
   <h1>申し込みが完了しました。</h1>
