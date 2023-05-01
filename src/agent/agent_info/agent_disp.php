dd492f96780f24360132648229c3a3c1b5cf3baa
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-05-01 10:04:24 +0900

    add:担当者情報追加

--- src/agent/agent_info/agent_disp.php
+++ src/agent/agent_info/agent_disp.php
@@ -1,118 +1,255 @@
-<?php 
+<?php
 
 require_once(dirname(__FILE__) . '/../../dbconnect.php');
 $pdo = Database::get();
-$sql = "SELECT * FROM clients WHERE client_id = :id";
-$stmt = $pdo->prepare($sql);
-$stmt->bindValue(":id", $_REQUEST["id"]);
-$stmt->execute();
-$agent= $stmt->fetch();
-
+$sql_1 = "SELECT * FROM clients WHERE client_id = :id ";
+$stmt1 = $pdo->prepare($sql_1);
+$stmt1->bindValue(":id", $_REQUEST["id"]);
+$stmt1->execute();
+$agent = $stmt1->fetch();
+$sql_2 = "SELECT * FROM label_client_relation INNER JOIN labels ON label_client_relation.label_id = labels.label_id WHERE label_client_relation.client_id = :id ";
+$stmt2 = $pdo->prepare($sql_2);
+$stmt2->bindValue(":id", $_REQUEST["id"]);
+$stmt2->execute();
+$labels = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+$sql_3 = "SELECT * FROM managers WHERE client_id = :id ";
+$stmt3 = $pdo->prepare($sql_3);
+$stmt3->bindValue(":id", $_REQUEST["id"]);
+$stmt3->execute();
+$manager = $stmt3->fetch();
 ?>
 
 <!DOCTYPE html>
 <html lang="ja">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet"
-    />
-    <link rel="stylesheet" href="../../vendor/tailwind/tailwind.output.css">
-    <title>boozer企業詳細</title>
-  </head>
-  <body>
-    <div
-      class="flex h-screen bg-gray-50"
-      :class="{ 'overflow-hidden': isSideMenuOpen}"
-    >
-      <!-- side banner -->
-      <aside class="z-20 flex-shrink-0 hidden w-64 overflow-y-auto bg-slate-500 md:block" >
-        <div class="py-4 text-gray-500">
-          <a
-            class="ml-6 text-lg font-bold text-gray-800 "
-            href="#"
-          >
-            SideBanner
-          </a>
-          <ul class="mt-6">
-            <li class="relative px-6 py-3">
-              <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800" href="../../admin/boozer_index.php">
-                <span class="ml-4">企業一覧</span>
-              </a>
-            </li>
-            <li class="relative px-6 py-3">
-              <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800" href="#">
-                <span class="ml-4">企業新規登録</span>
-              </a>
-            </li>
-            <li class="relative px-6 py-3">
-              <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800" href="../../admin/boozer_student.php">
-                <span class="ml-4">学生一覧</span>
-              </a>
-            </li>
-            <li class="relative px-6 py-3">
-              <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800" href="#">
-                <span class="ml-4">無効申請一覧</span>
-                <!-- 無効申請で絞り込みする -->
-              </a>
-            </li>
-          </ul>
-        </div>
-      </aside>
 
-      <div class="flex flex-col flex-1 w-full">
-        <main class="h-full pb-16 overflow-y-auto">
-          <div class="container grid px-6 mx-auto">
-            <h2 class="my-6 text-2xl font-semibold text-gray-700 ">企業詳細</h2>
-            <div class="w-full overflow-hidden rounded-lg shadow-xs">
-              <div class="w-full overflow-x-auto">
-                <table class="w-full whitespace-no-wrap">
-                  <thead>
-                    <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b">
-                      <th class="px-4 py-3">企業名</th>
-                      <th class="px-4 py-3">掲載期間</th>
-                      <th class="px-4 py-3">登録状態</th>
-                      <th class="px-4 py-3">操作</th>
-                    </tr>
-                  </thead>
-                  <tbody class="bg-white divide-y">
-                    <tr class="text-gray-700">
-                      <td class="px-4 py-3">
-                        <p class="font-semibold items-center text-sm"><?=$agent["service_name"]?></p>
-                      </td>
-                      <td class="px-4 py-3 text-sm">
-                        <?=$agent["started_at"]?>  ~  <?=$agent["ended_at"]?>
-                      </td>
-                      <td class="px-4 py-3 text-xs">
-                        <span
-                          class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full"
-                        >
-                        <!-- 色の設定はクラスの付加でjqueryで行う 登録無効（拒否）-->
-                          登録完了
-                        </span>
-                      </td>
-                      <td class="px-4 py-3">
-                        <div class="flex items-center space-x-4 text-sm">
-                          <button
-                            class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-blue-500 rounded-lg focus:outline-none focus:shadow-outline-gray"
-                            aria-label="Edit"
-                            data = <?=$agent["client_id"]?>
-                          >
-                            <a href="http://localhost:8080/agent/agent_info/agent_disp.php?id=<?=$agent["client_id"]?>">詳細</a>
-                          </button>
-                        </div>
-                      </td>
-                    </tr> 
-                  </tbody>
-                </table>
-              </div>
-              <div class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t sm:grid-cols-9">
-              </div>
-            </div>
-          </div>
-        </main>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+  <link rel="stylesheet" href="../../vendor/tailwind/tailwind.output.css">
+  <title>boozer企業詳細</title>
+</head>
+
+<body>
+  <div class="flex h-screen bg-gray-50" :class="{ 'overflow-hidden': isSideMenuOpen}">
+    <!-- side banner -->
+    <aside class="z-20 flex-shrink-0 hidden w-64 overflow-y-auto bg-slate-500 md:block">
+      <div class="py-4 text-gray-500">
+        <a class="ml-6 text-lg font-bold text-gray-800 " href="#">
+          SideBanner
+        </a>
+        <ul class="mt-6">
+          <li class="relative px-6 py-3">
+            <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800" href="../../admin/boozer_index.php">
+              <span class="ml-4">企業一覧に戻る</span>
+            </a>
+          </li>
+          <li class="relative px-6 py-3">
+            <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800" href="#">
+              <span class="ml-4">企業新規登録</span>
+            </a>
+          </li>
+          <li class="relative px-6 py-3">
+            <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800" href="../../admin/boozer_student.php">
+              <span class="ml-4">学生一覧</span>
+            </a>
+          </li>
+          <li class="relative px-6 py-3">
+            <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800" href="#">
+              <span class="ml-4">無効申請一覧</span>
+              <!-- 無効申請で絞り込みする -->
+            </a>
+          </li>
+        </ul>
       </div>
+    </aside>
+
+
+    <div class="flex flex-col flex-1 w-full">
+      <main class="h-full pb-16 overflow-y-auto">
+        <h1 class="my-6 text-2xl font-semibold text-gray-700 text-center"><?= $agent["service_name"] ?> 企業詳細</h1>
+        <p class="my-6 text-3xl font-semibold text-gray-700 text-center ">掲載状況：掲載中</p>
+        <!-- 掲載状況期間によって色で示す 掲載停止か掲載中-->
+        <div class="flex justify-center">
+          <table class="w-full mx-8 max-w-4xl bg-white shadow-md rounded-lg overflow-hidden">
+            <thead class="bg-blue-500 text-white">
+              <tr>
+                <th scope="col" class="px-6 py-3 text-left text-lg  font-medium uppercase tracking-wider">
+                  企業情報
+                </th>
+                <th scope="col" class="px-6 py-3 text-left text-lg font-medium uppercase tracking-wider">
+                  データ
+                </th>
+              </tr>
+            </thead>
+            <tbody class="divide-y divide-gray-200">
+              <tr>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-ms font-medium text-gray-900">
+                    企業名
+                  </div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-ms font-medium text-gray-900">
+                    <?= $agent["agent_name"] ?>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-ms font-medium text-gray-900">
+                    サービス名
+                  </div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-ms font-medium text-gray-900">
+                    <?= $agent["service_name"] ?>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-ms font-medium text-gray-900">
+                    掲載期間
+                  </div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-ms font-medium text-gray-900">
+                    <?= $agent["started_at"] ?> ~ <?= $agent["ended_at"] ?>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-ms font-medium text-gray-900">
+                    キャッチコピー
+                  </div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-ms font-medium text-gray-900">
+                    <?= $agent["catchphrase"] ?>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-ms font-medium text-gray-900">
+                    おすすめポイント1
+                  </div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-ms font-medium text-gray-900">
+                    <?= $agent["recommend_point1"] ?>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-ms font-medium text-gray-900">
+                    おすすめポイント2
+                  </div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-ms font-medium text-gray-900">
+                    <?= $agent["recommend_point2"] ?>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-ms font-medium text-gray-900">
+                    おすすめポイント3
+                  </div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-ms font-medium text-gray-900">
+                    <?= $agent["recommend_point3"] ?>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-ms font-medium text-gray-900">
+                    企業ロゴ
+                  </div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="flex-shrink-0 h-10 w-10">
+                    <img class="h-9 w-9" src="<?= $agent["logo_img"] ?>" alt="">
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="my-8 flex justify-center">
+          <table class="w-full mx-8 max-w-4xl bg-white shadow-md rounded-lg overflow-hidden">
+            <thead class="bg-blue-500 text-white">
+              <tr>
+                <th scope="col" class="px-6 py-3 text-left text-lg  font-medium uppercase tracking-wider">
+                  担当者情報
+                </th>
+                <th scope="col" class="px-6 py-3 text-left text-lg font-medium uppercase tracking-wider">
+                  データ
+                </th>
+              </tr>
+            </thead>
+            <tbody class="divide-y divide-gray-200">
+              <tr>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-ms font-medium text-gray-900">
+                    担当者名
+                  </div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-ms font-medium text-gray-900">
+                    <?= $manager["manager"] ?>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-ms font-medium text-gray-900">
+                    部署
+                  </div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-ms font-medium text-gray-900">
+                    <?= $manager["depart"] ?>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-ms font-medium text-gray-900">
+                    メールアドレス
+                  </div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-ms font-medium text-gray-900">
+                    <?= $manager["mail"] ?> 
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-ms font-medium text-gray-900">
+                    電話番号
+                  </div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-ms font-medium text-gray-900">
+                    <?= $manager["phone"] ?>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </main>
     </div>
-  </body>
-  </body>
+  </div>
+</body>
+</body>
+
 </html>
