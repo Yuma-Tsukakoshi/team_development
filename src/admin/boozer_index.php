d813999ac8e11284bbc51a4419a8fb28742187e2
平手美羽 <miyuh1@MacBook-Air-4.local>
2023-05-07 12:19:15 +0900

    取り敢えずリンクだけ作成

--- src/admin/boozer_index.php
+++ src/admin/boozer_index.php
@@ -38,6 +38,9 @@ $agents2 = $pdo->query("SELECT * FROM clients WHERE ended_at < CURDATE()")->fetc
             <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800" href="#">
               <span class="ml-4">企業一覧</span>
             </a>
+            <a href="http://localhost:8080/user/user_info/user_insert.php">
+              人数通知メールを各企業に送る
+            </a>
           </li>
           <li class="relative px-6 py-3">
             <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800" href="#">
