29cd686aba3ace7fb253efa2abb78a9b62c6c424
平手美羽 <miyuh1@MacBook-Air-4.local>
2023-05-09 20:28:29 +0900

    user→adminの修正

--- src/user/user_info/user_insert.done.php
+++ src/user/user_info/user_insert.done.php
@@ -1,27 +1,3 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>申し込み完了</title>
-    <!-- スタイルシート読み込み -->
-    <link rel="stylesheet" href="./admin/assets/styles/common.css">
-  <!-- Google Fonts読み込み -->
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link
-    href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Plus+Jakarta+Sans:wght@400;700&display=swap"
-    rel="stylesheet">
-  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script type="text/javascript" src="./../assets/js/jquery.zip2addr.js"></script>
-</head>
-<body>
-  <h1>申し込みが完了しました。</h1>
-<a href="../assets/index.html"><p>トップに戻る</p></a>
-</body>
-</html>
-
 <?php
 require_once(dirname(__FILE__) . './../../dbconnect.php');
 session_start();
@@ -29,7 +5,7 @@ $pdo=Database::get();
 
 try {
   $pdo->beginTransaction();
-  $stmt = $pdo->prepare("INSERT INTO admins(name,hurigana,sex,birthday,college,faculty,department,division,grad_year,prefecture,mail,phone) VALUES(:name,:hurigana,:sex,:birthday,:college,:faculty,:department,:division,:grad_year,:prefecture,:mail,:phone)");
+  $stmt = $pdo->prepare("INSERT INTO users(name,hurigana,sex,birthday,college,faculty,department,division,grad_year,prefecture,mail,phone) VALUES(:name,:hurigana,:sex,:birthday,:college,:faculty,:department,:division,:grad_year,:prefecture,:mail,:phone)");
   $stmt->execute([
     "name" => $_POST["name"],
     "hurigana" => $_POST['hurigana'],
@@ -52,9 +28,9 @@ try {
     $stmt->execute();
     $result = $stmt->fetch();
 
-    $stmt = $pdo->prepare("INSERT INTO admin_register_client(admin_id,client_id) VALUES(:admin_id,:client_id)");
+    $stmt = $pdo->prepare("INSERT INTO user_register_client(user_id,client_id) VALUES(:user_id,:client_id)");
     $stmt->execute([
-      "admin_id" => $id,
+      "user_id" => $id,
       "client_id" => $result['client_id'],
     ]);
   }
