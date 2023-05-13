9871a16deb0f9d75360dc0358b699776735df38e
HinanoOno <hinagon1231@icloud.com>
2023-05-13 22:53:18 +0900

    流れ星調整

--- src/user/assets/js/index.js
+++ src/user/assets/js/index.js
@@ -27,3 +27,35 @@ $(function(){
 
 // タイトルもフェードで入れようとしたら消えちゃった、、、！
 
+$(function() {
+  // スクロールイベント
+    $(window).scroll(function() {
+      // フェードアニメーションを呼び出す
+      fadeAnime();
+    });
+    
+    // フェードアニメーションの設定
+    function fadeAnime() {
+      // .fadeUpTriggerが付いた要素に対して
+      $('.star').each(function() {
+        // 縦位置を取得し-50pxして、変数targetに代入する
+        let target = $(this).offset().top -= 50;
+        // スクロール量を取得し、変数scrollに代入する
+        let scroll = $(window).scrollTop();
+        // 表示画面の高さを取得し、変数windowHeightに代入する
+        let windowHeight = $(window).height();
+        // 要素の縦位置から表示画面の高さを引いて200pxを足した数字よりscrollのほうが大きい場合
+        if(scroll > target - windowHeight ) {
+          // .fadeUpを追加する
+          $(this).addClass('move');
+        } else {
+          // そうでない場合は.fadeUpを削除する
+          $(this).removeClass('move');
+        }
+      });
+    };    
+  });
+  
+  
+  
+
