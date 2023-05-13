6ad15a60c7edaf8a642ea60f05e62db236c9de5e
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-05-13 23:59:11 +0900

    add:絞り込み機能AND条件

--- src/user/assets/js/filter.js
+++ src/user/assets/js/filter.js
@@ -1,47 +1,25 @@
-// $('input[name="filter[]"]').change(function() {
-//     // 選択されているチェックボックスの値を取得
-//     var filters = $('input[name="filter[]"]:checked').map(function() {
-//         return $(this).val();
-//     }).get();
-
-//     console.log(filters);
-
-//     // AJAXを使用してPHPスクリプトに送信する
-//     $.ajax({
-//         url: 'http://localhost:8080/user/user_agent_filter.php',
-//         type: 'post',
-//         data: {filter: filters},
-//         success: function(response) {
-//             // 成功したら以下の文が送られる
-//             console.log("OK");
-//         }
-//     });
-// });
-
 $('input[type="checkbox"]').change(function(){
     var filters = [];
     $('input[type="checkbox"]:checked').each(function(){
-    filters.push($(this).val());
-});
-
-if (filters.length > 0) {
-    $('ul li').hide();
-    // slideinで中に入れる
-    $('ul li').each(function(){
-    var options = $(this).data('options').split(' ');
-    var match = false;
-    for (var i = 0; i < options.length; i++) {
-        if ($.inArray(options[i], filters) !== -1) {
-            match = true;
-        }
-    }
-    if (match) {
-        $(this).show();
-    }
+        filters.push($(this).val());
     });
-} else {
-    $('ul li').show();
-    // slidein down とかで出し入れするといいかも
-}
-});
 
+    if (filters.length > 0) {
+        $('.agent-item').fadeOut(100);
+        $('.agent-item').each(function(){
+            var options = $(this).data('options').split(' ');
+            var match = true; // AND条件のために変更
+            for (var i = 0; i < filters.length; i++) { // 絞り込み条件のループを変更
+                if ($.inArray(filters[i], options) === -1) {
+                    match = false; // AND条件のために変更
+                    break; // 絞り込み条件に該当しない場合は、ループを終了する
+                }
+            }
+            if (match) {
+                $(this).fadeIn(300);
+            }
+        });
+    } else {
+        $('.agent-item').fadeIn(300);
+    }
+});
--- src/user/user_agent_list.php
+++ src/user/user_agent_list.php
@@ -1,14 +1,16 @@
 <?php
-session_start();
 require_once(dirname(__FILE__) . '/../dbconnect.php');
 // require_once(dirname(__FILE__) . '/user_agent_filter.php');
+session_start();
 
 $pdo = Database::get();
 $labels = $pdo->query("SELECT * FROM labels")->fetchAll(PDO::FETCH_ASSOC);
 $agent_labels = $pdo->query("SELECT * FROM label_client_relation INNER JOIN labels ON label_client_relation.label_id = labels.label_id")->fetchAll(PDO::FETCH_ASSOC);
 
-if(isset($_SESSION['clients'])){
-  $count=count($_SESSION['clients']);
+$agents = $pdo->query("SELECT * FROM clients WHERE ended_at >= CURDATE()")->fetchAll(PDO::FETCH_ASSOC);
+
+if (isset($_SESSION['clients'])) {
+  $count = count($_SESSION['clients']);
 }
 
 ?>
@@ -19,7 +21,7 @@ if(isset($_SESSION['clients'])){
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet"/>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
   <link rel="stylesheet" href="../vendor/tailwind/tailwind.css">
   <link rel="stylesheet" href="../user/assets/styles/search.css">
   <link rel="stylesheet" href="../user/assets/styles/header.css">
@@ -37,18 +39,19 @@ if(isset($_SESSION['clients'])){
       <span class="search-title-jpn">-エージェント検索-</span>
       <div class="search-title-border"></div>
     </div>
+
     <div class="cart">
       <div class="cart-num">
-        <?php if(isset($count)){?>
-          <?=$count?>
-        <?php }?>
+        <?php if (isset($count)) { ?>
+          <?= $count ?>
+        <?php } ?>
       </div>
       <a href="./user_cartlook.php">
         <img class="search-title-cart" src="../user/assets/img/728.png" alt="shopping_cart">
       </a>
       <div class="search-title-cart-border"></div>
     </div>
-    
+
 
   </section>
 
@@ -57,109 +60,111 @@ if(isset($_SESSION['clients'])){
       <div class="major">
         <h2 class="major-txt">専攻</h2>
         <img class="major-pencil-img" src="../user/assets/img/1263.png" alt="鉛筆の画像">
-        <?php for($i=1;$i<=2;$i++){ ?>
-        <input type="checkbox" id="major<?=$i?>" class="check-label" name="filter" value="<?=$labels
-        [$i-1]["label_id"] ?>">
-        <label for="major<?=$i?>" class="label-hover<?=$i?>"><?=$labels[$i-1]["label_name"] ?> </label>
-        <?php }?>
+        <?php for ($i = 1; $i <= 2; $i++) { ?>
+          <input type="checkbox" id="major<?= $i ?>" class="check-label" name="filter" value="<?= $labels[$i - 1]["label_id"] ?>">
+          <label for="major<?= $i ?>" class="label-hover<?= $i ?>"><?= $labels[$i - 1]["label_name"] ?> </label>
+        <?php } ?>
         <div class="major-border"></div>
       </div>
       <div class="contact">
         <h2 class="contact-txt">面談方法</h2>
         <img class="contact-mail-img" src="../user/assets/img/550.png" alt="メールの画像">
         <div class="contact-checkbox">
-          <?php for($i=3;$i<=5;$i++){ ?>
-          <input type="checkbox" id="contact<?=$i?>"  class="check-label contact-checkbox" name="filter" value="<?=$labels[$i-1]["label_id"] ?>">
-          <label for="contact<?=$i?>" class="label-hover"><?= $labels[$i-1]["label_name"] ?> </label>
-          <?php }?>
+          <?php for ($i = 3; $i <= 5; $i++) { ?>
+            <input type="checkbox" id="contact<?= $i ?>" class="check-label contact-checkbox" name="filter" value="<?= $labels[$i - 1]["label_id"] ?>">
+            <label for="contact<?= $i ?>" class="label-hover"><?= $labels[$i - 1]["label_name"] ?> </label>
+          <?php } ?>
           <div class="contact-border"></div>
         </div>
       </div>
       <div class="area">
         <h2 class="area-txt"> エリア </h2>
         <img class="area-point-img" src="../user/assets/img/686.png" alt="ピンの写真">
-        <?php for($i=6;$i<=9;$i++){ ?>
-        <input type="checkbox" id="area<?=$i?>" class="check-label" name="filter" value="<?=$labels[$i-1]["label_id"] ?>">
-        <label for="area<?=$i?>" class="label-hover"><?= $labels[$i-1]["label_name"] ?></label>
-        <?php }?>
+        <?php for ($i = 6; $i <= 9; $i++) { ?>
+          <input type="checkbox" id="area<?= $i ?>" class="check-label" name="filter" value="<?= $labels[$i - 1]["label_id"] ?>">
+          <label for="area<?= $i ?>" class="label-hover"><?= $labels[$i - 1]["label_name"] ?></label>
+        <?php } ?>
       </div>
       <button class="btn-big blue">検索</button>
     </form>
-    <div >
+    <div>
       <div class="results">
-          <img class="results-img" src="./assets/img/629.png" alt="虫眼鏡の画像">
-          <p class="results-txt"><span class="results-number"><?=count($agents)?></span>件ヒット</p>
+        <img class="results-img" src="./assets/img/629.png" alt="虫眼鏡の画像">
+        <p class="results-txt"><span class="results-number"><?= ?></span>件ヒット</p>
       </div>
       <div>
         <div class="my-16 ">
-          <?php foreach ($agents as $key => $agent){ ?>
-            <div class="top agent-list" date-options="
-            <?php foreach ($agent_labels as $agent_label){
-              if ($agent_label["client_id"] == $agent["client_id"]) {
-                print_r($agent_label["label_name"] . " "); 
+          <?php foreach ($agents as $key => $agent) { ?>
+            <div class="agent-item" data-options="<?php foreach ($agent_labels as $agent_label) {
+              if ($agent_label['client_id'] == $agent['client_id']) {
+                echo htmlspecialchars($agent_label['label_id']) . ' ';
               }
-            ?>">
-            <input type="hidden" value="<?=$agent['client_id']?>" class="client_id"> 
-            <img class="agent-img" src="<?=$agent["logo_img"]?>" alt="エージェント画像">
-            <div>
-              <h2 class="top-title"><?=$agent["service_name"]?></h2>
-              <div class="top-title-border"></div>
-              <div class="top-description">
-                <h3 class="top-description-title"><?=$agent["catchphrase"]?></h3>
-                <p><?=$agent["recommend_point1"]?></p>
-                <p><?=$agent["recommend_point2"]?></p>
-                <p><?=$agent["recommend_point3"]?></p>
+            } ?>">
+            <input type="hidden" value="<?= $agent['client_id'] ?> " class="client_id">
+            <div class="top agent-list">
+              <img class="agent-img" src="<?= $agent["logo_img"] ?>" alt="エージェント画像">
+              <div>
+                <h2 class="top-title"><?= $agent["service_name"] ?></h2>
+                <div class="top-title-border"></div>
+                <div class="top-description">
+                  <h3 class="top-description-title"><?= $agent["catchphrase"] ?></h3>
+                  <p><?= $agent["recommend_point1"] ?></p>
+                  <p><?= $agent["recommend_point2"] ?></p>
+                  <p><?= $agent["recommend_point3"] ?></p>
+                </div>
+                <div class="top-description-border"></div>
+                </div>
               </div>
-              <div class="top-description-border"></div>
+              <div class="bottom">
+                <div class="labels">
+                  <?php foreach ($agent_labels as $agent_label) { ?>
+                    <?php if ($agent_label["client_id"] == $agent["client_id"]) { ?>
+                      <span class="label-major">
+                        <?= $agent_label["label_name"] ?>
+                      </span>
+                    <?php } ?>
+                  <?php } ?>
+                </div>
+                <div class="block">
+                  <button class="btn-big cyan add-button" id="cart<?= $key + 1 ?>" value="<?= $key ?>">カートに追加する</button>
+                  <button class="btn-big blue see-details" id="agent<?= $key + 1 ?>">詳細を見る→</button>
+                </div>
+              </div>
             </div>
-          </div>
-          <div class="bottom">
-            <div class="labels">
-              <?php foreach($agent_labels as $agent_label){ ?>
-              <?php if($agent_label["client_id"]==$agent["client_id"]){ ?>
-              <span class="label-major">
-                <?=$agent_label["label_name"]?>
-              </span>
-              <?php }?>
-              <?php }?>
-            </div>
-            <div class="block">
-              <button class="btn-big cyan add-button" id="cart<?=$key+1?>" value="<?=$key?>">カートに追加する</button>
-              <button class="btn-big blue see-details" id="agent<?=$key+1?>">詳細を見る→</button>
-            </div>
-          </div>
+          <?php } ?>
         </div>
-        <?php } ?>
       </div>
     </div>
   </main>
   <script>
-  $(function(){
-            $('.add-button').on('click', function(event){
-              $index=this.value
+    $(function() {
+      $('.add-button').on('click', function(event) {
+        $index = this.value
 
-              console.log($index)
-                $.ajax({
-                    type: "POST",
-                    url: "./user_cartin.php",
-                    data: {
-                      id: $index,
-                      client_id:$('.client_id').eq($index).val(),
-                      
-                    },
-                    dataType : "json",
-                    scriptCharset: 'utf-8'
-                }).done(function(data){
-                  console.log(data);
+        console.log($index)
+        $.ajax({
+          type: "POST",
+          url: "./user_cartin.php",
+          data: {
+            id: $index,
+            client_id: $('.client_id').eq($index).val(),
 
-                  $('.cart-num').text(data)
-                  $('.add-button').eq($index).prop("disabled", true);
-                 //背景グレーとか調整する
-                }).fail(function(XMLHttpRequest, textStatus, errorThrown){
-                    alert(errorThrown);
-                });
-            })
-          })
+          },
+          dataType: "json",
+          scriptCharset: 'utf-8'
+        }).done(function(data) {
+          console.log(data);
+
+          $('.cart-num').text(data)
+          $('.add-button').eq($index).prop("disabled", true);
+          //背景グレーとか調整する
+
+        }).fail(function(XMLHttpRequest, textStatus, errorThrown) {
+          alert(errorThrown);
+        });
+      })
+    })
   </script>
 </body>
+
 </html>
