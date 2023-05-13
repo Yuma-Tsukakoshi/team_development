fd64a7aefdee0adc0bada356b3c1aeda5e8a7254
Goki Kanno <goki.kanno@gmail/com>
2023-05-13 17:39:08 +0900

    無効申請とヘッダー背景変え

--- src/agent/agent_insert_reason.php
+++ src/agent/agent_insert_reason.php
@@ -1,8 +1,28 @@
 <!-- SQL文を発行するページ -->
+<?php
+session_start();
+require_once(dirname(__FILE__) . '../../dbconnect.php');
+$pdo = Database::get();
 
-<!-- INSERT 文 -->
-
-<!-- UPDATE文 -->
 
 
-<!-- Header location リダイレクト agent_boozer.php -->
+// <!-- INSERT 文 -->
+$sql = "INSERT INTO invalid_reason(user_id,client_id,reason) VALUES(:uid,:client_id,:reason)";
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(":uid", $_SERVER["id"]);
+$stmt->bindValue(":client_id", $_SESSION["client_id"]);
+$stmt->execute();
+// <!-- UPDATE文 -->
+$sql2 = "UPDATE user_register_client SET valid=1 WHERE user_id= :uid AND client_id =:client_id;";
+$stmt2=$pdo->prepare($sql2);
+$stmt2->bindValue(":uid", $_SERVER["id"]);
+$stmt2->bindValue(":client_id", $_SESSION["client_id"]);
+$stmt2->execute();
+
+// <!-- Header location リダイレクト agent_boozer.php -->
+
+header('Location:http://localhost:8080/agent/agent_boozer.php');
+exit();
+
+
+?>
