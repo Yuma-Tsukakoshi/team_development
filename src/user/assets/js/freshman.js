be3c94cb80435071d4dd040b8a1399243eaf51ba
平手美羽 <miyuh1@MacBook-Air-4.local>
2023-05-14 14:41:03 +0900

    トップページ8割完成

--- src/user/assets/js/freshman.js
+++ src/user/assets/js/freshman.js
@@ -23,3 +23,15 @@ $(document).ready(function() {
     });
   });
   
+  $(function(){
+    $(window).scroll(function (){
+        $('.star-fadein').each(function(){
+            var position = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > position - windowHeight + 200){
+              $(this).addClass('active');
+            }
+        });
+    });
+  });
