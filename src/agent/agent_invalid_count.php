bee794437fb2f80e6d09a7bd3087bfd2d1e2d9e8
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-05-14 14:29:42 +0900

    fix:validのエラー回避

--- src/agent/agent_invalid_count.php
+++ src/agent/agent_invalid_count.php
@@ -2,7 +2,7 @@
 require_once(dirname(__FILE__) . '/../dbconnect.php');
 
 $pdo = Database::get();
-$sql = "SELECT COUNT(*) FROM users INNER JOIN user_register_client AS relation ON users.id = relation.user_id WHERE valid = 1 AND relation.client_id = :id ORDER BY updated_at DESC";
+$sql = "SELECT COUNT(*) FROM users INNER JOIN user_register_client AS relation ON users.id = relation.user_id WHERE relation.valid = 1 AND relation.client_id = :id ORDER BY updated_at DESC";
 $stmt = $pdo->prepare($sql);
 $stmt->bindValue(":id", $_SESSION["id"]);
 $stmt->execute();
--- src/agent/agent_invalid_student.php
+++ src/agent/agent_invalid_student.php
@@ -4,7 +4,7 @@ require_once(dirname(__FILE__) . '/../dbconnect.php');
 require_once(dirname(__FILE__) . '/agent_invalid_count.php');
 
 $pdo = Database::get();
-$sql = "SELECT * FROM users INNER JOIN user_register_client AS relation ON users.id = relation.user_id WHERE valid = 1 AND relation.client_id = :id ORDER BY updated_at DESC";
+$sql = "SELECT * FROM users INNER JOIN user_register_client AS relation ON users.id = relation.user_id WHERE relation.valid = 1 AND relation.client_id = :id ORDER BY updated_at DESC";
 $stmt = $pdo->prepare($sql);
 $stmt->bindValue(":id", $_SESSION["id"]);
 $stmt->execute();
