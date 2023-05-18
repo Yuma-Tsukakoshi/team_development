5a48bc58cd3057d0c3db3057f0880b55875a544a
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-05-18 18:37:15 +0900

    fix:ユーザー画面の企業をexit=1のみだすに変更

--- src/user/user_agent_list.php
+++ src/user/user_agent_list.php
@@ -13,7 +13,7 @@ $labels = $pdo->query("SELECT * FROM labels")->fetchAll(PDO::FETCH_ASSOC);
 $agent_labels = $pdo->query("SELECT * FROM label_client_relation INNER JOIN labels ON label_client_relation.label_id = labels.label_id")->fetchAll(PDO::FETCH_ASSOC);
 
 
-$agents = $pdo->query("SELECT * FROM clients WHERE ended_at >= CURDATE()")->fetchAll(PDO::FETCH_ASSOC);
+$agents = $pdo->query("SELECT * FROM clients WHERE ended_at >= CURDATE() AND exist=1")->fetchAll(PDO::FETCH_ASSOC);
 
 
 
