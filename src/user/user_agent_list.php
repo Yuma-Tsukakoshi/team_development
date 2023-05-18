af069cfed6b938350865f12618198d22c4f72dfb
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-05-18 09:44:19 +0900

    add:絞り込み機能復活

--- src/user/user_agent_list.php
+++ src/user/user_agent_list.php
@@ -36,13 +36,12 @@ $agents = $pdo->query("SELECT * FROM clients WHERE ended_at >= CURDATE()")->fetc
 
 <body>
   <?php include(dirname(__FILE__) . '/../components/header.php'); ?>
-  <section class="search" >
+  <section class="search">
     <div>
       <h1 class="search-title">SEARCH</h1>
       <span class="search-title-jpn">-エージェント検索-</span>
       <div class="search-title-border"></div>
     </div>
-
     <div class="cart">
       <div class="cart-num">
         <?php if (isset($count)) { ?>
@@ -54,8 +53,6 @@ $agents = $pdo->query("SELECT * FROM clients WHERE ended_at >= CURDATE()")->fetc
       </a>
       <div class="search-title-cart-border"></div>
     </div>
-
-
   </section>
   <div class="overlay" id="js-overlay"></div>
 
@@ -67,12 +64,11 @@ $agents = $pdo->query("SELECT * FROM clients WHERE ended_at >= CURDATE()")->fetc
       <div class="message">商品をかごに追加しました</div>
     </div>
     <div class="modal-link">
-      <a  href="./user_info/user_insert.php"><p class="link-message">申し込みはこちら→</p></a>
+      <a href="./user_info/user_insert.php">
+        <p class="link-message">申し込みはこちら→</p>
+      </a>
     </div>
-    
-
   </div>
-
 
   <main class="grid grid-cols-2">
     <form method="post" action="" class="m-8 w-3">
@@ -80,10 +76,10 @@ $agents = $pdo->query("SELECT * FROM clients WHERE ended_at >= CURDATE()")->fetc
         <h2 class="major-txt">専攻</h2>
         <img class="major-pencil-img" src="../user/assets/img/1263.png" alt="鉛筆の画像">
         <div class="major-checkbox">
-        <?php for ($i = 1; $i <= 2; $i++) { ?>
-          <input type="checkbox" id="major<?= $i ?>" class="check-label" name="filter" value="<?= $labels[$i - 1]["label_id"] ?>">
-          <label for="major<?= $i ?>" class="label-hover<?= $i ?>"><?= $labels[$i - 1]["label_name"] ?> </label>
-        <?php } ?>
+          <?php for ($i = 1; $i <= 2; $i++) { ?>
+            <input type="checkbox" id="major<?= $i ?>" class="check-label" name="filter" value="<?= $labels[$i - 1]["label_id"] ?>">
+            <label for="major<?= $i ?>" class="label-hover<?= $i ?>"><?= $labels[$i - 1]["label_name"] ?> </label>
+          <?php } ?>
         </div>
         <div class="major-border"></div>
       </div>
@@ -107,7 +103,7 @@ $agents = $pdo->query("SELECT * FROM clients WHERE ended_at >= CURDATE()")->fetc
         <?php } ?>
       </div>
     </form>
-    <div>
+    <div class="results-wrapper">
       <div class="results">
         <img class="results-img" src="./assets/img/629.png" alt="虫眼鏡の画像">
         <p class="results-txt"><span class="results-number"></span>件ヒット</p>
@@ -116,23 +112,23 @@ $agents = $pdo->query("SELECT * FROM clients WHERE ended_at >= CURDATE()")->fetc
         <div class="my-16 ">
           <?php foreach ($agents as $key => $agent) { ?>
             <div class="agent-item" data-options="<?php foreach ($agent_labels as $agent_label) {
-              if ($agent_label['client_id'] == $agent['client_id']) {
-                echo htmlspecialchars($agent_label['label_id']) . ' ';
-              }
-            } ?>">
-            <input type="hidden" value="<?= $agent['client_id'] ?> " class="client_id">
-            <div class="top agent-list">
-              <img class="agent-img" src="<?= $agent["logo_img"] ?>" alt="エージェント画像">
-              <div>
-                <h2 class="top-title"><?= $agent["service_name"] ?></h2>
-                <div class="top-title-border"></div>
-                <div class="top-description">
-                  <h3 class="top-description-title"><?= $agent["catchphrase"] ?></h3>
-                  <p><?= $agent["recommend_point1"] ?></p>
-                  <p><?= $agent["recommend_point2"] ?></p>
-                  <p><?= $agent["recommend_point3"] ?></p>
-                </div>
-                <div class="top-description-border"></div>
+                                                    if ($agent_label['client_id'] == $agent['client_id']) {
+                                                      echo htmlspecialchars($agent_label['label_id']) . ' ';
+                                                    }
+                                                  } ?>">
+              <input type="hidden" value="<?= $agent['client_id'] ?> " class="client_id">
+              <div class="top agent-list">
+                <img class="agent-img" src="<?= $agent["logo_img"] ?>" alt="エージェント画像">
+                <div>
+                  <h2 class="top-title"><?= $agent["service_name"] ?></h2>
+                  <div class="top-title-border"></div>
+                  <div class="top-description">
+                    <h3 class="top-description-title"><?= $agent["catchphrase"] ?></h3>
+                    <p><?= $agent["recommend_point1"] ?></p>
+                    <p><?= $agent["recommend_point2"] ?></p>
+                    <p><?= $agent["recommend_point3"] ?></p>
+                  </div>
+                  <div class="top-description-border"></div>
                 </div>
               </div>
               <div class="bottom">
@@ -151,72 +147,71 @@ $agents = $pdo->query("SELECT * FROM clients WHERE ended_at >= CURDATE()")->fetc
                 </div>
               </div>
             </div>
-          </div>
-          <?php }?>
-
         </div>
+      <?php } ?>
       </div>
+    </div>
     </div>
   </main>
   <script>
-  $(function(){
-            //スクロールすると上部に固定させるための設定を関数でまとめる
-            function FixedAnime() {
-              var headerH = $('.search').outerHeight(true);
-              var scroll = $(window).scrollTop();
-              if (scroll+30 >= headerH){//headerの高さ以上になったら
-                  $('.search').addClass('move');//fixedというクラス名を付与
-                }else{//それ以外は
-                  $('.search').removeClass('move');//fixedというクラス名を除去
-                }
+    $(function() {
+      //スクロールすると上部に固定させるための設定を関数でまとめる
+      function FixedAnime() {
+        var headerH = $('.search').outerHeight(true);
+        var scroll = $(window).scrollTop();
+        if (scroll + 30 >= headerH) { //headerの高さ以上になったら
+          $('.search').addClass('move'); //fixedというクラス名を付与
+        } else { //それ以外は
+          $('.search').removeClass('move'); //fixedというクラス名を除去
+        }
+      }
+      // 画面をスクロールをしたら動かしたい場合の記述
+      $(window).scroll(function() {
+        FixedAnime(); /* スクロール途中からヘッダーを出現させる関数を呼ぶ*/
+      });
+
+      // ページが読み込まれたらすぐに動かしたい場合の記述
+      $(window).on('load', function() {
+        FixedAnime(); /* スクロール途中からヘッダーを出現させる関数を呼ぶ*/
+      });
+
+      $('.add-button').on('click', function(event) {
+        $index = this.value
+
+        console.log($index)
+        $.ajax({
+          type: "POST",
+          url: "./user_cartin.php",
+          data: {
+            id: $index,
+            client_id: $('.client_id').eq($index).val(),
+
+          },
+          dataType: "json",
+          scriptCharset: 'utf-8'
+        }).done(function(data) {
+          console.log(data);
+          $('.modal-content').fadeIn();
+          $('.overlay').fadeIn();
+
+          // クリックイベント全てに対しての処理
+          $(document).on('click touchend', function(event) {
+            // 表示したポップアップ以外の部分をクリックしたとき
+            if (!$(event.target).closest('.modal-content').length) {
+              $('.modal-content').fadeOut();
+              $('.overlay').fadeOut();
             }
-            // 画面をスクロールをしたら動かしたい場合の記述
-            $(window).scroll(function () {
-              FixedAnime();/* スクロール途中からヘッダーを出現させる関数を呼ぶ*/
-            });
+          });
 
-            // ページが読み込まれたらすぐに動かしたい場合の記述
-            $(window).on('load', function () {
-              FixedAnime();/* スクロール途中からヘッダーを出現させる関数を呼ぶ*/
-            });
+          $('.cart-num').text(data)
+          $('.add-button').eq($index).prop("disabled", true);
+          //背景グレーとか調整する
 
-            $('.add-button').on('click', function(event){
-              $index=this.value
-
-              console.log($index)
-                $.ajax({
-                    type: "POST",
-                    url: "./user_cartin.php",
-                    data: {
-                      id: $index,
-                      client_id:$('.client_id').eq($index).val(),
-                      
-                    },
-                    dataType : "json",
-                    scriptCharset: 'utf-8'
-                }).done(function(data){
-                  console.log(data);
-                  $('.modal-content').fadeIn();
-                  $('.overlay').fadeIn(); 
-              
-                  // クリックイベント全てに対しての処理
-                  $(document).on('click touchend', function(event) {
-                    // 表示したポップアップ以外の部分をクリックしたとき
-                    if (!$(event.target).closest('.modal-content').length) {
-                      $('.modal-content').fadeOut();
-                      $('.overlay').fadeOut();
-                    }
-                  });
-
-                  $('.cart-num').text(data)
-                  $('.add-button').eq($index).prop("disabled", true);
-                 //背景グレーとか調整する
-                 
-                }).fail(function(XMLHttpRequest, textStatus, errorThrown){
-                    alert(errorThrown);
-                });
-            })
-          })
+        }).fail(function(XMLHttpRequest, textStatus, errorThrown) {
+          alert(errorThrown);
+        });
+      })
+    })
   </script>
 </body>
 
