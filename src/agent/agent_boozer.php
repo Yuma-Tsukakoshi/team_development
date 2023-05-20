c6af0ea19530c508c392e1beb2455733d6e43b9f
平手美羽 <miyuh1@MacBook-Air-4.local>
2023-05-20 15:19:48 +0900

    admin画面学生削除機能

--- src/agent/agent_boozer.php
+++ src/agent/agent_boozer.php
@@ -8,7 +8,7 @@ if (isset($_SESSION['agent_sort'])) {
 }
 
 $pdo = Database::get();
-$sql = "SELECT * FROM users INNER JOIN user_register_client AS r ON users.id = r.user_id WHERE r.client_id = :id  and agent_is_valid=true ORDER BY updated_at DESC";
+$sql = "SELECT * FROM users INNER JOIN user_register_client AS r ON users.id = r.user_id WHERE r.client_id = :id  ORDER BY updated_at DESC";
 $stmt = $pdo->prepare($sql);
 $stmt->bindValue(":id", $_SESSION["id"]);
 $stmt->execute();
