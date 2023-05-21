c77875cba22bcc283e0f159adf8f13af7512d6cf
平手美羽 <miyuh1@MacBook-Air-4.local>
2023-05-21 22:47:44 +0900

    ハンバーガーメニュー修正必要

--- src/user/assets/js/index.js
+++ src/user/assets/js/index.js
@@ -44,3 +44,19 @@ $(window).scroll(function (){
   fadeAnime();/* アニメーション用の関数を呼ぶ*/
 });// ここまで画面をスクロールをしたら動かしたい場合の記述
 
+
+// ハンバーガーメニュー
+$(function() {
+  $('.menu-btn').click(function() {
+      $(this).toggleClass('active');
+
+      if ($(this).hasClass('active')) {
+          $('.gnavi__sp-style').addClass('active');
+      } else {
+          $('.gnavi__sp-style').removeClass('active');
+      }
+  });
+});
+
+
+
