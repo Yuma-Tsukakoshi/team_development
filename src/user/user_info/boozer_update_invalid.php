5d147705db49b258e0c2f0f50c7a06110f8915d5
平手美羽 <miyuh1@MacBook-Air-4.local>
2023-05-24 11:12:30 +0900

    無効申請承認→学生にメール通知

--- src/user/user_info/boozer_update_invalid.php
+++ src/user/user_info/boozer_update_invalid.php
@@ -16,18 +16,19 @@ $stmt->execute();
 
 // メール機能
 // clientへのメール
-$sql1 = "SELECT mail FROM managers WHERE client_id = :client_id";
-$stmt1 = $pdo->prepare($sql);
-$stmt1->bindValue(":client_id", $inputId);
-$stmt1->execute();
-$manager_mail = $stmt->fetchAll(PDO::FETCH_COLUMN);
+// $sql1 = "SELECT mail FROM managers WHERE client_id = :client_id";
+// $stmt1 = $pdo->prepare($sql1);
+// $stmt1->bindValue(":client_id", $inputId);
+// $stmt1->execute();
+// $manager_mail = $stmt1->fetchAll(PDO::FETCH_COLUMN);
+;
 
 // userへのメール
-$sql2 = "SELECT mail FROM users WHERE user_id = :uid";
-$stmt2 = $pdo->prepare($sql);
+$sql2 = "SELECT mail FROM users WHERE id = :uid";
+$stmt2 = $pdo->prepare($sql2);
 $stmt2->bindValue(":uid", $_SESSION["uid"]);
 $stmt2->execute();
-$user = $stmt->fetch(PDO::FETCH_ASSOC);
+$user = $stmt2->fetch(PDO::FETCH_ASSOC);
 
 $headers = 'From: admin@mail' . "\r\n" .
 'Reply-To: admin@mail' . "\r\n" .
@@ -43,26 +44,26 @@ function send_email($to, $subject, $message, $headers) {
 }
 
 // user宛のメール
-$to_user= $user;
-$subject_user = "【株式会社boozer】お申し込みありがとうございます";
-$subject_user = "【株式会社boozer】お申し込みありがとうございます";
+$to_user= $user["mail"];
+$subject_user = "【株式会社boozer】申請拒否のお知らせ";
+$subject_user = "【株式会社boozer】お申し込みありがとうございました";
 $message_user = "※このメールはシステムからの自動返信です\n\n";
 $message_user .= "株式会社boozerでの新規登録ありがとうございました。\n\n";
-$message_user .= "お申し込みいただいた企業から担当者よりご連絡いたしますので\n";
-$message_user .= "今しばらくお待ちくださいませ。\n\n";
+$message_user .= "入力の不備のため、申請を拒否させていただきました。\n";
+$message_user .= "ご不明点があればご連絡ください\n\n";
 
 send_email($to_user, $subject_user, $message_user, $headers);
 
 // manager宛のメール
-$to_managers = $manager_mail;
-$subject_client = "【株式会社boozer】学生登録のお知らせ";
-$message_client = "お世話になっております。\n";
-$message_client .= "株式会社boozerでございます。\n\n";
-$message_client .= "CRAFTを通して学生より貴社の就活エージェントにお申込みいただいたことを通知いたします。詳しくはCRAFTよりご覧ください。\n\n";
-$message_client .= "なお、営業時間は平日9時〜18時となっております。\n";
-$message_client .= "時間外のお問い合わせは翌営業日にご連絡差し上げます。\n\n";
-$message_client .= "ご理解・ご了承の程よろしくお願い致します。";
+// $to_client = $manager_mail;
+// $subject_client = "【株式会社boozer】学生登録のお知らせ";
+// $message_client = "お世話になっております。\n";
+// $message_client .= "株式会社boozerでございます。\n\n";
+// $message_client .= "CRAFTを通して学生より貴社の就活エージェントにお申込みいただいたことを通知いたします。詳しくはCRAFTよりご覧ください。\n\n";
+// $message_client .= "なお、営業時間は平日9時〜18時となっております。\n";
+// $message_client .= "時間外のお問い合わせは翌営業日にご連絡差し上げます。\n\n";
+// $message_client .= "ご理解・ご了承の程よろしくお願い致します。";
 
-send_email($to_client, $subject_client, $message_client, $headers);
+// send_email($to_client, $subject_client, $message_client, $headers);
 
 ?>
