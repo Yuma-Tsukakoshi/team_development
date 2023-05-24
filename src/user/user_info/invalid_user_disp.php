5976ecfb71cd37ab4db5af8ca53d4f0e22c399db
平手美羽 <miyuh1@MacBook-Air-4.local>
2023-05-24 10:45:47 +0900

    書く場所移動

--- src/user/user_info/invalid_user_disp.php
+++ src/user/user_info/invalid_user_disp.php
@@ -26,48 +26,6 @@ $stmt3->bindValue(":uid", $_REQUEST["id"]);
 $stmt3->execute();
 $invalid_agents = $stmt3->fetchAll();
 
-// メール機能
-$sql4 = "SELECT mail FROM managers WHERE client_id IN (SELECT client_id FROM user_register_client WHERE user_id = :id)";
-$stmt4 = $pdo->prepare($sql4);
-$stmt4->bindValue(":id", $_REQUEST["id"]);
-$stmt4->execute();
-$manager_mail = $stmt4->fetchAll(PDO::FETCH_COLUMN);
-
-$headers = 'From: admin@mail' . "\r\n" .
-'Reply-To: admin@mail' . "\r\n" .
-'X-Mailer: PHP/' . phpversion();
-
-// 宛先と件名、メッセージをそれぞれ設定してメール送信関数を呼び出す
-function send_email($to, $subject, $message, $headers) {
-  if (mail($to, $subject, $message, $headers)) {
-  } else {
-    echo "メールの送信に失敗しました。\n";
-    echo "エラー情報: " . error_get_last()['message'];
-  }
-}
-
-// user宛のメール
-$to_user= $user["mail"] ;
-$subject_user = "【株式会社boozer】お申し込みありがとうございます";
-$subject_user = "【株式会社boozer】お申し込みありがとうございます";
-$message_user = "※このメールはシステムからの自動返信です\n\n";
-$message_user .= "株式会社boozerでの新規登録ありがとうございました。\n\n";
-$message_user .= "お申し込みいただいた企業から担当者よりご連絡いたしますので\n";
-$message_user .= "今しばらくお待ちくださいませ。\n\n";
-
-send_email($to_user, $subject_user, $message_user, $headers);
-
-// manager宛のメール
-$to_managers = $manager_mail;
-$subject_client = "【株式会社boozer】学生登録のお知らせ";
-$message_client = "お世話になっております。\n";
-$message_client .= "株式会社boozerでございます。\n\n";
-$message_client .= "CRAFTを通して学生より貴社の就活エージェントにお申込みいただいたことを通知いたします。詳しくはCRAFTよりご覧ください。\n\n";
-$message_client .= "なお、営業時間は平日9時〜18時となっております。\n";
-$message_client .= "時間外のお問い合わせは翌営業日にご連絡差し上げます。\n\n";
-$message_client .= "ご理解・ご了承の程よろしくお願い致します。";
-
-send_email($to_client, $subject_client, $message_client, $headers);
 
 
 ?>
