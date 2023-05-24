ac555a5e6ce042ffcd321c9d6cd602fa6cce3bcd
平手美羽 <miyuh1@MacBook-Air-4.local>
2023-05-24 23:43:28 +0900

    拒否したら企業にも送れた

--- src/user/assets/js/agent_send_invalid.js
+++ src/user/assets/js/agent_send_invalid.js
@@ -1,20 +1,22 @@
 // 申請拒否
 $(document).ready(function() {
   $('#invalid_btn').click(function() {
-    var inputVal = $(this).attr('data');
-    var inputId = $(this).attr('client'); 
-    console.log(inputVal);
-    console.log(inputId);
+    var data = this.getAttribute('data'); 
+    var clientId = this.getAttribute('client'); 
     $.ajax({
       url: 'http://localhost:8080/user/user_info/boozer_update_invalid.php', // 送信先のPHPファイルのパス
       type: 'POST',
       data: {
-        input: inputVal,
-        inputId: inputId
-      }, // 送信するデータ。キーは'inputVal'、値はinputVの値
-      success: function(data) {
+        data: data,
+        clientId: clientId,
+      }, 
+      contentType: "application/x-www-form-urlencoded",
+      processData: true,
+      // 送信するデータ。キーは'data'、値はinputVの値
+      success: function() {
         // 送信が成功した場合の処理
         console.log(data); // レスポンスデータをコンソに表示
+        console.log(clientId);
       },
       error: function(xhr, status, error) {
         // 送信が失敗した場合の処理
@@ -22,5 +24,7 @@ $(document).ready(function() {
       }
     });
     location.reload();
+    // return false;
   });
 });
+
--- src/user/user_info/boozer_update_invalid.php
+++ src/user/user_info/boozer_update_invalid.php
@@ -1,16 +1,49 @@
 <?php
 session_start();
+
 require_once(dirname(__FILE__) . '/../../dbconnect.php');
 $pdo = Database::get();
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$inputVal = $_POST['input'];
-$inputId = $_POST['inputId'];
+$data = $_POST['data'];
+$clientId = $_POST['clientId'];
 
 $sql = "UPDATE user_register_client SET valid = :valid WHERE user_id = :uid AND client_id = :client_id;";
 $stmt = $pdo->prepare($sql);
-$stmt->bindValue(":valid", $inputVal);
+$stmt->bindValue(":valid", $data);
 $stmt->bindValue(":uid", $_SESSION["uid"]);
-$stmt->bindValue(":client_id", $inputId);
+$stmt->bindValue(":client_id", $clientId);
 $stmt->execute();
 
+// 企業へのメールアドレスを取得
+$sql3 = "SELECT mail FROM managers WHERE client_id = :client_id";
+$stmt3 = $pdo->prepare($sql3);
+$stmt3->bindValue(":client_id", $clientId);
+$stmt3->execute();
+$manager = $stmt3->fetch(PDO::FETCH_ASSOC);
+
+$headers = 'From: admin@mail' . "\r\n" .
+    'Reply-To: admin@mail' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+// 企業宛のメール
+$to_manager = $manager["mail"];
+$subject_manager = "【株式会社boozer】申請承認のお知らせ";
+$message_manager = "※このメールはシステムからの自動返信です\n\n";
+$message_manager .= "株式会社boozerでの新規登録ありがとうございました。\n\n";
+$message_manager .= "無効申請を承認させていただきました。\n";
+$message_manager .= "詳しくはCRAFTのページをご覧ください\n\n";
+$message_manager .= "ご不明点あれば連絡ください\n\n";
+
+// 宛先と件名、メッセージをそれぞれ設定してメール送信関数を呼び出す
+function send_email($to, $subject, $message, $headers) {
+    if (mail($to, $subject, $message, $headers)) {
+    } else {
+        echo "メールの送信に失敗しました。\n";
+        echo "エラー情報: " . error_get_last()['message'];
+    }
+}
+
+send_email($to_manager, $subject_manager, $message_manager, $headers);
 ?>
+
