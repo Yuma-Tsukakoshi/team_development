959dc60df9e5592e6c5b0a206eede3bfa1202b56
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-05-28 23:21:17 +0900

    fix:ログアウト後自動でもどる

--- src/admin/boozer_auth/boozer_logout.php
+++ src/admin/boozer_auth/boozer_logout.php
@@ -4,8 +4,8 @@ require_once(dirname(__FILE__) . '/../../dbconnect.php');
 
 session_start();
 $_SESSION = array();//セッションの中身をすべて削除
-session_destroy();//セッションを破壊
-?>
+session_destroy(); //セッションを破壊
 
-<p>ログアウトしました。</p>
-<a href="boozer_login.php">ログインへ</a>
+header('Location: http://localhost:8080/admin/boozer_auth/boozer_login.php');
+exit();
+?>
